c61dd6c46536d97051f87942e282937460de4dff
Nithishkumarr <nithishrajesh18me@nieit.ac.in>
2022-10-28 17:58:23 +0530

    part 2 update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import Heading from './components/App';
-import Footer from './components/Footer';
-import Note from './components/Note';
+import App from "./components/App"
 
 
 const container=document.getElementById('root');
 const root = createRoot(container);
 root.render(<div>
-<Heading />
-<Note />
-<Footer />
+<App />
 </div>
 );
 
